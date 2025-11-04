94fe7551810945a130873bc054c04dec0e2d6069
Derek Velzy <derekvelzy@velzy.local>
2025-11-04 13:48:17 -0800

    export metadata from portfolio page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,10 +1,17 @@
 // Package imports
+import type { Metadata } from "next";
+
 // Custom imports
 import Page from "~/components/Page/Page";
 import Portfolio from "~/components/Portfolio/Portfolio";
 
 // Do not index this page
-export const robots: { index: false, follow: false } = { index: false, follow: false };
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 
 export default function PortfolioPage() {
   return (
